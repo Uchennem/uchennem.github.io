30ca282fe61a2554a17a06e7e06f9ea5dcf2442a
Uche <uchesoranye@gmail.com>
2022-09-19 10:53:06 -0600

    entered

--- wdd130/portal/js/assignment_portal.js
+++ wdd130/portal/js/assignment_portal.js
@@ -1,3 +1,7 @@
+let date = new Date();
 
 let currentdate = document.lastModified;
+let year = date.getFullYear();
+
+document.querySelector('.year').textContent = year;
 document.getElementById('foot').textContent = `Last Updated: ${currentdate}`;
