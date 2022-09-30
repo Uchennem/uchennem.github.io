16a57a816dfc54b8da434a206928c82df2141adc
Uche <uchesoranye@gmail.com>
2022-09-30 10:22:41 -0600

    dijhweioywedao

---
+++ wdd230/debug_learn/trials/scripts/main3.js
@@ -0,0 +1,48 @@
+const grades = ['A', 'B', 'A'];
+
+function convertGrade(grade) {
+    let points = 0;
+    if (grade === 'A') {
+        points = 4;
+    } 
+    else if (grade === 'B') {
+        points = 3;
+    }
+    return points
+}
+
+const gpaPoints = grades.map(convertGrade)
+
+console.log(gpaPoints);
+
+const pointsTotal = gpaPoints.reduce(function (total, item) {
+    return total + item; 
+});
+let gpa = pointsTotal / gpaPoints.length;
+console.log(gpa);
+
+const words = ["watermelon", "Peach", "Apple", "Tomato", "Grape"];
+const shortwords = words.filter(function (item) {
+    return item.length < 6;
+});
+
+console.log(shortwords);
+
+const buttonElement = document.getElementById("submitButton");
+
+function copyInput() {
+  const inputElement = document.getElementById("inputBox");
+  const outputElement = document.getElementById("output");
+  outputElement.innerHTML = inputElement.value;
+}
+
+buttonElement.addEventListener("click", copyInput);
+
+const numbs = [1, 2, 3, 4, 5];
+
+document.querySelector('#output').textContent = nums;
+function getEvens(item) {
+    if (item % 2 === 0) {
+        return item;
+    }
+}
