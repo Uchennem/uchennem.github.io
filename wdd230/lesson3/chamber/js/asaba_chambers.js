dd99bb7300f8da33c3d837c9f8d1068ef0070c02
Uche <uchesoranye@gmail.com>
2022-10-15 12:17:23 -0600

    Yes

--- wdd230/lesson3/chamber/js/asaba_chambers.js
+++ wdd230/lesson3/chamber/js/asaba_chambers.js
@@ -14,3 +14,5 @@ function toggleMenu() {
 
 const clickChecker = document.getElementById("hamburger")
 clickChecker.onclick = toggleMenu;
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
