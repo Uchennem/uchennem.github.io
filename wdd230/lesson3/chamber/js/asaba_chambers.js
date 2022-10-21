b2ca74b460ed7566a6a8f6682cea47aa71d3c7f0
Uche <uchesoranye@gmail.com>
2022-10-20 19:30:53 -0600

    finally

--- wdd230/lesson3/chamber/js/asaba_chambers.js
+++ wdd230/lesson3/chamber/js/asaba_chambers.js
@@ -3,10 +3,13 @@ let date = new Date();
 let currentdate = document.lastModified;
 let year = date.getFullYear();
 
+
+
 document.querySelector('.full_date').textContent = new Date().toDateString();
 document.querySelector('.year').textContent = year;
 document.getElementById('foot').textContent = `Last Updated: ${currentdate}`;
 
+// This function creates a hamburger menu on clicking the side bar
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
     console.log("it worked")
@@ -14,7 +17,7 @@ function toggleMenu() {
 const clickChecker = document.getElementById("hamburger")
 clickChecker.onclick = toggleMenu;
 
-
+// This Displays banner on site on Monday and Tuesday
 let dayNumber = date.getDay();
 let banner = document.querySelector('#bannertext');
 
@@ -27,3 +30,19 @@ if(dayNumber == 1 || dayNumber == 2){
 }
 
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+
+// This does calculation for temp and wind chill
+
+let temp = document.querySelector('#temp').textContent;
+let speed = document.querySelector('#speed').textContent;
+let chill;
+
+if ((temp <= 50) || (speed > 3))
+{
+    chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(speed,0.16)) + (0.4275*temp*Math.pow(speed,0.16)));
+    document.querySelector('#chill').textContent = chill;
+}
+
+else{
+    document.querySelector('#chill').textContent = 'N/A';
+}
