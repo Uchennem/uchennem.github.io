b3b1ef31623b9bcc6bd03765fcdb5ce072721bdc
Uche <uchesoranye@gmail.com>
2022-10-31 13:11:54 -0600

    Uche

--- wdd230/lazyload/main.js
+++ wdd230/lazyload/main.js
@@ -1,45 +1,56 @@
-// const io = new IntersectionObserver (
-// (entries, io) => {
-// entries.forEach(entry => {
-// console.log(entries);
-// })
-// },
-// {
-// threshold: [.5]
-// }
-// );
+const image = document.querySelectorAll('img');
 
-// image.forEach(image => {
-// io.observe(image);
+// const io = new IntersectionObserver(
+//     (entries, io) => {
+//         entries.forEach(entry => {
+//             console.log(entries);
+//         })
+        
+//     }, 
+//     {
+//         threshold: [.5]
+//     }
+// )
+
+
+// image.forEach(image =>{
+//     io.observe(image);
 // })
 
 const pimages = document.querySelectorAll('[data-src]');
 const options = {
-threshold: 1,
-rootMargin: "0px 0px 100px 0px"
+    threshold: 1,
+    rootMargin: '0px 0px 100px 0px'
 }
 
 function preloadImage(img){
-const source = img.getAttribute('data-src');
-if (!source) {
-return;
+    const source = img.getAttribute('data-src');
+    if (!source) {
+        return;
+    }
+
+    img.src = source;
 }
 
-img.src = source;
-}
+const io = new IntersectionObserver(
+    (entries, io) => {
+        entries.forEach(entry => {
+            if(!entry.isIntersecting){
+                return;
+            }
+            else{
+                preloadImage(entry.target);
+                io.unobserve(entry.target);
+            }
+        });
+        
+    }, 
+    
+        options
+    
+);
 
-const io = new IntersectionObserver (
-(entries, io) => {
-entries.forEach(entry => {
-if (!entry.isIntersecting) {
-return;
-} else {
-preloadImage(entry.target);
-io.unobserve(entry.target);
-}
-});
-},options);
-
-pimages.forEach(image => {
-io.observe(image);
+pimages.forEach(image =>{
+    io.observe(image);
 })
+
