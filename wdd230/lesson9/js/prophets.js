c0abc580c9c094b7ae7f706c95004e4c94b99dce
Uche <uchesoranye@gmail.com>
2022-11-11 09:04:08 -0700

    managemenet

--- wdd230/lesson9/js/prophets.js
+++ wdd230/lesson9/js/prophets.js
@@ -21,10 +21,20 @@ getProphets(requestURL);
 function displayProphets(item) {
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
+    let dob = document.createElement('p');
+    let pob = document.createElement('p');
+    let img = document.createElement('img');
 
-    h2.textContent = item.name + " " + item.lastname;
+    h2.textContent = `${item.name} ${item.lastname}`;
+    dob.textContent = item.birthdate;
+    pob.textContent = item.birthplace;
+    img.setAttribute('src', item.imageurl);
+    img.setAttribute('alt', item.name + " " + item.lastname);
 
     card.appendChild(h2);
+    card.appendChild(img);
+    card.appendChild(dob);
+    card.appendChild(pob);
     document.querySelector('.cards').appendChild
         (card);
 };
