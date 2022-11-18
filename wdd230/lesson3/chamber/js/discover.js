222faebcb73c26c05ec5a6ff3664f17e114d6e4c
Uche <uchesoranye@gmail.com>
2022-11-18 09:26:38 -0700

    The final solution

--- wdd230/lesson3/chamber/js/discover.js
+++ wdd230/lesson3/chamber/js/discover.js
@@ -56,27 +56,19 @@ pimages.forEach(image =>{
 
 
 
-function supriseMe() {
+
+if (!localStorage.getItem('recentVisit')) {
+    localStorage.setItem('recentVisit', Date.now());
+    document.querySelector('#Visit').textContent = 'This is your first visit';
+       
+    }
+else {
     let lastDate = localStorage.getItem('recentVisit');
     let newDate = Date.now();
 
     let dayDifference = Math.abs(newDate - lastDate);
     let actualDays = Math.floor(dayDifference / 1000 / 3600 / 24);
-    if (actualDays < 2) {
-        document.querySelector('.currentVisit').textContent = 'This is your first visit';
-    }
-    else {
-        document.querySelector('#visit').textContent = "It's been " + actualDays + " days since you last Visited";
-    }
+    document.querySelector('#visit').textContent = "It's been " + actualDays + " days since your last Visit";
     localStorage.setItem('recentVisit', Date.now());
-}
-
-
-if (!localStorage.getItem('recentVisit')) {
-        localStorage.setItem('recentVisit', Date.now());
-       
-    }
-else {
-    supriseMe();
 }
 
