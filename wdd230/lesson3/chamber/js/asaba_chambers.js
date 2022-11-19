d8290eed183e36b7d4e2a4574a75f154e8286e3d
Uche <uchesoranye@gmail.com>
2022-11-19 12:26:51 -0700

    erea

--- wdd230/lesson3/chamber/js/asaba_chambers.js
+++ wdd230/lesson3/chamber/js/asaba_chambers.js
@@ -29,7 +29,6 @@ if(dayNumber == 1 || dayNumber == 2){
     style.display = 'block';
 }
 
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
 
 // This does calculation for temp and wind chill
 
@@ -37,12 +36,39 @@ let temp = document.querySelector('#temp').textContent;
 let speed = document.querySelector('#speed').textContent;
 let chill;
 
-if ((temp <= 50) || (speed > 3))
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=2349276&appid=bfd498807a1d14d42fbb52b98ae038ae";
+
+const getWeather = async () => {
+    const response = await fetch(apiURL);
+    const jsObject = await response.json();
+    console.log(jsObject);
+    let kelvin = jsObject.main.temp;
+    let metrePerSec = jsObject.wind.speed;
+    //(k - 273.15) * 1.8000 + 32.00
+    let farenheit = (kelvin - 273.15) * 1.8 + 32;
+    let speed = metrePerSec * 2.237;
+    console.log(speed);
+    document.querySelector("#temp").textContent = Math.round(farenheit);
+    document.querySelector("#speed").textContent = Math.ceil(speed);
+
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('.icon').textContent = iconsrc;
+    document.querySelector('.icon').setAttribute('src', iconsrc);
+    document.querySelector('.icon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc
+
+    if ((farenheit <= 50) || (speed > 3))
 {
-    chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(speed,0.16)) + (0.4275*temp*Math.pow(speed,0.16)));
+    chill = Math.round((35.74 + (0.6215 * farenheit))-(35.75 * Math.pow(speed,0.16)) + (0.4275*farenheit*Math.pow(speed,0.16)));
     document.querySelector('#chill').textContent = chill;
 }
 
 else{
     document.querySelector('#chill').textContent = 'N/A';
 }
+};
+
+getWeather()
+
+
