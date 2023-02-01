646c558932a4f51ca79f1c4943aa27dec3137129
Uche <uchesoranye@gmail.com>
2023-02-01 11:47:16 -0700

    GQSFCTHSFG

---
+++ wdd330/swapi/SwapiHelper.js
@@ -0,0 +1,133 @@
+const baseUrl = "https://swapi.dev/api/";
+
+export default class SwapiHelper {
+    constructor(outputId, filmId, loadingId) {
+        this.outputId = outputId;
+        this.outputElement = document.getElementById(outputId);
+        this.filmId = filmId;
+        this.filmElement = document.getElementById(filmId);
+        this, loadingId = loadingId;
+        this.loadingElement = document.getElementById(loadingId);
+        this.films = [];
+        this.init();
+    }
+
+    async init() {
+        this.outputElement.style.display = "none";
+        this.loadingElement.style.display = "block";
+        this.films = await this.makeRequest(baseUrl + "films");
+        this.films = this.films.results;
+        console.log(this.films)
+        this.outputElement.style.display = "initial";
+        this.loadingElement.style.display = "none";
+        this.clickableList(this.films, this.filmId, this.filmSelected.bind(this));
+    }
+
+    async makeRequest(url) {
+        try {
+            const response = await fetch(url);
+            if (response.ok) {
+                return await response.json();
+            }
+
+            else {
+                const error = await response.text();
+                throw new Error(error)
+            }
+        }
+        catch (err) {
+            console.log(err);
+        }
+
+    }
+    clickableList(list, elementId, callback) {
+        const element = document.getElementById(elementId)
+        element.innerHTML = list.map((film) => `<li>${film.title}</li>`).join("");
+        element.addEventListener("click", (e) => {
+            console.log(e.target);
+            this.setActive(element, e.target);
+            callback(e.target.innerText);
+        });
+    }
+
+    async filmSelected(filmTitle) {
+        try {
+            const film = this.films.find((item) =>
+                item.title === filmTitle);
+            if (!film) {
+                throw new Errror("Film not found");
+
+            }
+            this.outputElement.innerHTML = this.pageTemplate();
+            this.outputElement.querySelector(".film-name").innerHTML = film.title;
+            this.outputElement.querySelector(".crawl").innerText = film.opening_crawl;
+            const planets = await this.getListDetails(film.planets);
+            this.renderList(planets, this.planetTemplate, ".film-planets");
+            const starships = await this.getListDetails(film.starships);
+            this.renderList(starships, this.starshipsTemplate, ".film-starships");
+        }
+        catch (err) {
+            console.log(err);
+        }
+        
+    }
+
+    renderList(list, template, outputId) {
+        const element = document.querySelector(outputId);
+        element.innerHTML = "";
+        const htmlString = list.map((item) =>
+        template(item));
+        element.innerHTML = htmlString.join("");
+    }
+
+    pageTemplate() {
+        return `<h2 class="film-name"></h2>
+        <p class = "crawl"></p>
+        <section class="planets">
+            <h3>planets</h3>
+            <ul class="detail-list film-planets"></ul>
+        </section>
+        <section>
+            <h3>Starships</h3>
+            <ul class="detail-list film-starships"></ul>
+        </section>`;
+    }
+
+    setActive(parent, target) {
+        const children = [...parent.childNodes]
+        children.forEach((node) => {
+            node.classList.remove("active");
+        });
+        target.classList.add("active");
+    }
+
+    async getListDetails(list) {
+        const details = await Promise.all(list.map((url) =>
+            this.makeRequest(url)));
+        console.log(details);
+        return details;
+    }
+
+    planetTemplate(planet) {
+        return `<li>
+        <h4 class="planet-name">${planet.name}</h4>
+        <p>Climate: ${planet.climate}</p>
+        <p>Terrain: ${planet.terrain}</p>
+        <p>Year: ${planet.orbital_period}</p>
+        <p>Day: ${planet.rotation_period}</p>
+        <p>Population: ${planet.population}</p>
+        </li>`;
+    }
+
+    starshipsTemplate(starship) {
+        return `<li>
+        <h4>${starship.name}</h4>
+        <p>Model: ${starship.model}</p>
+        <p>Manufacturer: ${starship.manufacturer}</p>
+        <p>Cost in Credits: ${starship.cost_in_credits}</p>
+        <p>Atmosphering Speed: ${starship.max_atmosphering_speed}</p>
+        <p>Crew Capacity: ${starship.crew}</p>
+        </li>`;
+    }
+}
+
---
+++ wdd330/swapi/main.js
@@ -0,0 +1,10 @@
+import SwapiHelper from "./SwapiHelper.js";
+
+const myHelper = new SwapiHelper("output", "films", "loading");
+
+// myHelper.init();
+
+function size(a) {
+    return size.length
+    }
+    console.log(size(['a', 'b'])); //should return 2
